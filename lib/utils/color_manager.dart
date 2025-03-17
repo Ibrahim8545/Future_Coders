8077dd5df811648aa3f26e1d1eb05c99aaba96af
mohammedashraf16 <mohammedashraf1692003@gmail.com>
2025-03-17 17:05:21 +0200

    add color manager

---
+++ lib/utils/color_manager.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+abstract class ColorManager {
+  static Color primary800 = const Color(0xFF0A638F);
+  static Color primary500 = const Color(0xFFA9DEF9);
+  static Color primary50 = const Color(0xFFE7F6FD);
+  static Color primary100 = const Color(0xFFCFEDFC);
+  static Color primary200 = const Color(0xFFA0DBF8);
+  static Color primary300 = const Color(0xFF70C9F5);
+  static Color primary400 = const Color(0xFF40B7F2);
+  static Color primary600 = const Color(0xFF0D84BF);
+  static Color primary700 = const Color(0xFF0A638F);
+  static Color primary900 = const Color(0xFF032130);
+  static Color primary950 = const Color(0xFF021018);
+  static Color appBarTitleColor = const Color(0xFF262626);
+
+
+
+  static Color black500 = const Color(0xff262626);
+  static Color black50 = const Color(0xffF2F2F2);
+  static Color black950 = const Color(0xff0D0D0D);
+  static Color black100 = const Color(0xffE6E6E6);
+  static Color black200 = const Color(0xffCCCCCC);
+  static Color black300 = const Color(0xffB3B3B3);
+  static Color black400 = const Color(0xff999999);
+  static Color black600 = const Color(0xff666666);
+  static Color black700 = const Color(0xff4D4D4D);
+  static Color black800 = const Color(0xff333333);
+  static Color black900 = const Color(0xff1A1A1A);
+  static Color transparent =  Colors.transparent;
+
+  static const Color green500 = Color(0XFF0CCF57);
+  static const Color green50 = Color(0XFFE7FEF0);
+  static const Color green100 = Color(0XFFCFFCE0);
+  static const Color green200 = Color(0xff9FF9C1);
+  static const Color green300 = Color(0xff6EF7A3);
+  static const Color green400 = Color(0xff6EF7A3);
+  static const Color green600 = Color(0xff0BC151);
+  static const Color green700 = Color(0xff08913D);
+  static const Color green800 = Color(0xff066028);
+  static const Color green900 = Color(0xff033014);
+
+  // new colors
+  static Color secondary500 = const Color(0xffFF99C9);
+  static Color secondary50 = const Color(0xffFFE5F1);
+  static Color secondary100 = const Color(0xffFFCCE4);
+  static Color secondary200 = const Color(0xffFF99C9);
+  static Color secondary300 = const Color(0xffFF66AD);
+  static Color secondary400 = const Color(0xffFF3392);
+  static Color secondary600 = const Color(0xffCC005F);
+  static Color secondary700 = const Color(0xff990047);
+  static Color secondary800 = const Color(0xff660030);
+  static Color secondary900 = const Color(0xff330018); // color with 80% opacity
+
+
+  static Color grey1 = const Color(0xff707070);
+  static Color grey2 = const Color(0xff797979);
+  static Color white = const Color(0xffFFFFFF);
+  static Color error = const Color(0xffe61f34); // red color
+
+
+
+}
