2ca07edb34310208937cf341a4bf34fdab3e7669
Ibrahim8545 <ibrahimmostafa122020@gmail.com>
2025-03-20 07:33:33 +0200

    add image to setting tab

--- lib/features/main/settings/settings_tab.dart
+++ lib/features/main/settings/settings_tab.dart
@@ -4,36 +4,51 @@ import 'package:courseapp/utils/color_manager.dart';
 import 'package:courseapp/utils/font_manager.dart';
 import 'package:courseapp/utils/values_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SettingsTab extends StatelessWidget {
   const SettingsTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Stack(
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+        Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 78.0),
-              child: Text("الاعدادات",
-                  style: TextStyle(
-                      color: ColorManager.black500,
-                      fontSize: 24,
-                      fontWeight: FontWeightManager.bold)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 78.0),
+                  child: Text("الاعدادات",
+                      style: TextStyle(
+                          color: ColorManager.black500,
+                          fontSize: 24,
+                          fontWeight: FontWeightManager.bold)),
+                ),
+              ],
             ),
+            const SizedBox(height: AppSize.s40),
+            CustomRowSettingsWidget(
+                iconPath: IconsAssets.settingIcon, title: "الملف الشخصي"),
+            CustomRowSettingsWidget(
+                iconPath: IconsAssets.editProfile, title: "تعديل الملف الشخضي"),
+            CustomRowSettingsWidget(
+                iconPath: IconsAssets.favorite, title: "المفضلات"),
+            CustomRowSettingsWidget(
+                iconPath: IconsAssets.payIcon, title: "الدفع"),
+            CustomRowSettingsWidget(
+                iconPath: IconsAssets.helpIcon, title: "المساعده والتواصل"),
+            CustomRowSettingsWidget(
+                iconPath: IconsAssets.logout, title: "تسجيل الخروج"),
           ],
         ),
-        const SizedBox(height: AppSize.s40),
-        CustomRowSettingsWidget(iconPath: IconsAssets.settingIcon,title: "الملف الشخصي"),
-        CustomRowSettingsWidget(iconPath: IconsAssets.editProfile,title: "تعديل الملف الشخضي"),
-        CustomRowSettingsWidget(iconPath: IconsAssets.favorite,title: "المفضلات"),
-        CustomRowSettingsWidget(iconPath: IconsAssets.payIcon,title: "الدفع"),
-        CustomRowSettingsWidget(iconPath: IconsAssets.helpIcon,title: "المساعده والتواصل"),
-        CustomRowSettingsWidget(iconPath: IconsAssets.logout,title: "تسجيل الخروج"),
+        Positioned(
+          child: Image.asset(
+            'assets/images/Mask group.png',
+          ),
+        )
       ],
     );
   }
 }
-
