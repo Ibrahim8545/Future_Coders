8aefc80d3a7be1204f739201fccd068c9bd20548
mohammedashraf16 <mohammedashraf1692003@gmail.com>
2025-03-21 17:12:07 +0200

    refactor code.

--- lib/config/routes/routes.dart
+++ lib/config/routes/routes.dart
@@ -2,6 +2,8 @@ class Routes {
   static const String mainRoute = "/main";
   static const String signIn = '/signIn';
   static const String signUp = '/signUp';
+  static const String forgetPassword = '/forgetPassword';
   static const String profileView = '/profileView';
   static const String editProfileView = '/editProfileView';
+  static const String helpView = '/helpView';
 }
